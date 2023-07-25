57d5a06c21b6b6884f6608b4243aa7619290c7ef
Mario <mario.aguila_70@hotmail.com>
2023-07-25 13:15:55 -0400

    Login y menu

--- app-registros/src/app/app-routing.module.ts
+++ app-registros/src/app/app-routing.module.ts
@@ -10,7 +10,15 @@ const routes: Routes = [
   {
     path: 'folder/:id',
     loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+  },  {
+    path: 'login',
+    loadChildren: () => import('./auth/login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'main',
+    loadChildren: () => import('./menu/main/main.module').then( m => m.MainPageModule)
   }
+
 ];
 
 @NgModule({
